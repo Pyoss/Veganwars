9ba3940c7c6d5d293978b78bbd74d32ede8f8bc5
Pyoss <yisgit@gmail.com>
2019-05-27 23:07:39 +0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
  
 #!/bin/bash
-until python -m test; do
-    export ERROR_MSG="'myscript.py' crashed with exit code $?. Restarting..." &>2
+until python -m bot; do
+    export ERROR_MSG="'vwars' crashed with exit code $?. Restarting..." &>2
     python -m bot_watcher
     sleep 1
 done
