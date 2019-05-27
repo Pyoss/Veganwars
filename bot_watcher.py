c1140e111fb45e9aa358d1e892acb75f911b6c22
Pyoss <yisgit@gmail.com>
2019-05-27 23:06:05 +0300

    Update bot_watcher.py

--- bot_watcher.py
+++ bot_watcher.py
@@ -1,7 +1,8 @@
 import sys
 import telebot
 import os
+import config
 
-bot = telebot.TeleBot('777849028:AAFKdy8OJcLn37H7A8bJVsSCTSB-5S37zf4', threaded=False)
+bot = telebot.TeleBot(config.token, threaded=False)
 admin_id = 197216910
 bot.send_message(admin_id, os.environ['ERROR_MSG'])
