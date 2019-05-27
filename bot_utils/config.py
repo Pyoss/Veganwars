b8d26204e5cce10b7b8e876fb3ec6e8cbbc64b32
Pyoss <yisgit@gmail.com>
2019-05-27 23:16:35 +0300

    Update config.py

--- bot_utils/config.py
+++ bot_utils/config.py
@@ -2,6 +2,7 @@ token = '525874076:AAHrpOItjryLt_N2-H3aQLXNtopyo5i1W_k'
 turn_time = 60
 build_time = 60
 admin_id = 197216910
+main_chat_id = -1001290009216
 test_ability = []
 test_items = []
 test_weapon = []
