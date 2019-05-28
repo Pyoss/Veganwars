50717ab2cc5f8883949994dc14ec1faa6fd16a46
Pyoss <yisgit@gmail.com>
2019-05-28 12:04:31 +0000

    Bot watcher message

--- bot_watcher.py
+++ bot_watcher.py
@@ -4,4 +4,5 @@ import os
 from bot_utils import config
 
 bot = telebot.TeleBot(config.token, threaded=False)
+bot.send_message(config.main_chat_id, 'Бот неожидано прервал работу и будет перезапущен; \n\n Код ошибки:')
 bot.send_message(config.main_chat_id, os.environ['ERROR_MSG'])
