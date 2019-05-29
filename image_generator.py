3e87f33d12db51d3ac49abe153be309e27150416
Pyoss <yisgit@gmail.com>
2019-05-29 15:07:10 +0300

    Image generation rework.

--- image_generator.py
+++ image_generator.py
@@ -2,89 +2,120 @@ from PIL import Image
 import io
 
 
-def create_dungeon_image(background_image, image_tuple_list):
-    background_image = Image.open(background_image)
-    width, height = background_image.size
+class ImageObject:
+    def __init__(self, image_file: Image, height_type: str, padding_tuple: tuple):
+        self.image = image_file
+        self.width, self.height = image_file.size
+        self.height_type = height_type
+        self.padding_width = padding_tuple[0]
+        self.padding_height = padding_tuple[1]
+        self.background_place = None
 
-    size_height_dict = {'standard': int(height*2/3),
-                        'high': int(height*5/6),
-                        'low': int(height*2/5)}
-    sized_lists_dict = {'high': [], 'standard': [], 'low': []}
+    def resize(self, planned_height: int):
+        image_modifier = planned_height/(self.height - self.padding_height)
+        self.height = int(self.height * image_modifier)
+        self.width = int(self.width * image_modifier)
+        self.image = self.image.resize((self.width, self.height))
+        self.padding_width = int(self.padding_width * image_modifier)
+        self.padding_height = int(self.padding_height * image_modifier)
 
-    for tpl in image_tuple_list:
-        sized_lists_dict[tpl[1]].append((tpl[0], tpl[2]))
-
-    common_pudding_list = []
-    if len(sized_lists_dict['standard']) == len(sized_lists_dict['low']) == len(sized_lists_dict['high']) == 1:
-        common_pudding_list = ['high', 'standard', 'low']
-    if len(sized_lists_dict['standard']) == len(sized_lists_dict['low']) == 1:
-        common_pudding_list = ['standard', 'low']
-    elif len(sized_lists_dict['high']) == len(sized_lists_dict['standard']) == 1:
-        common_pudding_list = ['high', 'standard']
-    elif len(sized_lists_dict['high']) == len(sized_lists_dict['low']) == 1 and len(sized_lists_dict['standard']) == 0:
-        common_pudding_list = ['high', 'low']
-
-    class Common:
-        common_pudding = 0
-        common_item_len = sum([len(sized_lists_dict[item]) for item in common_pudding_list]) if common_pudding_list else 0
-
-    for key in sized_lists_dict:
-        current_padding = Common.common_pudding if key in common_pudding_list else 0
-        current_height = size_height_dict[key]
-        for image_tuple in sized_lists_dict[key]:
-            print(image_tuple)
-            items_len = len(sized_lists_dict[key]) if key not in common_pudding_list else Common.common_item_len
-            width_padding = int(width/(items_len + 1))
-            current_padding += width_padding
-            if key in common_pudding_list:
-                Common.common_pudding += width_padding
-            pasting_image = image_tuple[0]
-            pasting_width, pasting_height = pasting_image.size
-            image_width_padding, image_top_padding = image_tuple[1]
-            print(image_tuple[1])
-            new_width = int(pasting_width*(current_height/(pasting_height-image_top_padding)))
-            pasting_image = pasting_image.resize((new_width, current_height + int(image_top_padding*(current_height/(pasting_height-image_top_padding)))))
-            background_image.paste(pasting_image, (int(current_padding-new_width/2) - image_width_padding,
-                                                   height - current_height - int(image_top_padding*(current_height/(pasting_height-image_top_padding)))), mask=pasting_image)
-
-    return io_from_PIL(background_image)
+    def get_center(self) -> int:
+        return int(self.width/2) + self.padding_width
 
 
-def create_duel_image(image_tuple_list):
-    background_image = Image.open('D:\YandexDisk\Veganwars\Veganwars\\files\images\\backgrounds\Duel.png')
-    width, height = background_image.size
+class ImageBackground:
+    def __init__(self, image_file: Image):
+        self.image_file = image_file
+        self.width, self.height = image_file.size
 
-    size_height_dict = {'standard': int(height*2/3),
-                        'high': int(height*5/6),
-                        'low': int(height*2/5)}
-    ######
-    image_tuple = image_tuple_list[0]
-    pasting_image = image_tuple[0]
-    pasting_width, pasting_height = pasting_image.size
-    image_width_padding, image_top_padding = image_tuple[2]
-    current_height = size_height_dict[image_tuple[1]]
-    new_width = int(pasting_width*(current_height/pasting_height))
-    new_height = current_height + int(image_top_padding*(current_height/pasting_height))
-    pasting_image = pasting_image.resize((new_width, new_height))
-    background_image.paste(pasting_image,
-                           (0, height - current_height - int(image_top_padding*(current_height/pasting_height))),
-                           mask=pasting_image)
-    ######
-    image_tuple = image_tuple_list[1]
-    pasting_image = image_tuple[0]
-    pasting_width, pasting_height = pasting_image.size
 
-    image_width_padding, image_top_padding = image_tuple[2]
-    current_height = size_height_dict[image_tuple[1]]
-    new_width = int(pasting_width*(current_height/pasting_height))
-    new_height = current_height + int(image_top_padding*(current_height/pasting_height))
-    pasting_image = pasting_image.resize((new_width, new_height))
-    background_image.paste(pasting_image,
-                           (width-new_width,
-                            height - current_height - int(image_top_padding*(current_height/pasting_height))),
-                           mask=pasting_image)
-    return io_from_PIL(background_image)
+class ImageConstructor:
+    def __init__(self, background: Image, image_objects: tuple):
+        self.background_image = background
+        self.size_heights = self.get_size_heights()
+        self.sized_lists_dict = {'high': [], 'standard': [], 'low': []}
+        self.image_objects = [ImageObject(*image_object) for image_object in image_objects]
+        self.common_pudding_list = []
+        self.padding_markers_dict = None
 
+    def get_size_heights(self):
+        return {'standard': int(self.background_image.height*2/3),
+                        'high': int(self.background_image.height*5/6),
+                        'low': int(self.background_image.height*2/5)}
+
+    def get_common_height_tier_list(self):
+        for image_object in self.image_objects:
+            image_object.resize(self.size_heights[image_object.height_type])
+            self.sized_lists_dict[image_object.height_type].append(image_object)
+
+        if len(self.sized_lists_dict['standard']) == len(self.sized_lists_dict['low']) == len(self.sized_lists_dict['high']) == 1:
+            self.common_pudding_list = ['high', 'standard', 'low']
+        if len(self.sized_lists_dict['standard']) == len(self.sized_lists_dict['low']) == 1:
+            self.common_pudding_list = ['standard', 'low']
+        elif len(self.sized_lists_dict['high']) == len(self.sized_lists_dict['standard']) == 1:
+            self.common_pudding_list = ['high', 'standard']
+        elif len(self.sized_lists_dict['high']) == len(self.sized_lists_dict['low']) == 1 and len(
+                self.sized_lists_dict['standard']) == 0:
+            self.common_pudding_list = ['high', 'low']
+
+    def setup_markers(self):
+
+        class PaddingMarker:
+
+            def __init__(self):
+                self.padding = 0
+                self.items_len = 1
+
+        common_marker = PaddingMarker()
+        self.padding_markers_dict = {key: common_marker if key in self.common_pudding_list else PaddingMarker()
+                                     for key in self.sized_lists_dict.keys()}
+
+        for image_object in self.image_objects:
+            marker = self.padding_markers_dict[image_object.height_type]
+            marker.items_len += 1
+
+    def get_background_coord(self, image_object):
+        padding_marker = self.padding_markers_dict[image_object.height_type]
+        padding_coord = int(self.background_image.width/padding_marker.items_len)
+        padding_marker.padding += padding_coord
+        return padding_marker.padding - image_object.get_center()
+
+    def create_image(self):
+        self.get_common_height_tier_list()
+        self.setup_markers()
+
+        for value in self.sized_lists_dict.values():
+            for image_object in value:
+                self.background_image.image_file.paste(image_object.image,
+                                                       (self.get_background_coord(image_object),
+                                                        self.background_image.height - image_object.height),
+                                                       mask=image_object.image)
+
+        return self.background_image.image_file
+
+
+class DuelImage(ImageConstructor):
+
+    def __init__(self, background: Image, image_objects: tuple):
+        ImageConstructor.__init__(self, background=background, image_objects=image_objects)
+        self.left_corner = int(self.background_image.width/3)
+        self.right_corner = int(self.background_image.width/3*2)
+        self.left_taken = False
+
+    def get_background_coord(self, image_object):
+        if self.left_taken:
+            corner = self.right_corner
+            return corner
+        else:
+            corner = self.left_taken
+            self.left_taken = True
+            return corner + image_object.get_center()
+
+
+def create_dungeon_image(background, image_tuples):
+    constructor = DuelImage(ImageBackground(Image.open(background)), image_tuples)
+    image = constructor.create_image()
+    return io_from_PIL(image)
 
 def io_from_PIL(image):
     img_byte_arr = io.BytesIO()
@@ -92,10 +123,17 @@ def io_from_PIL(image):
     img_byte_arr = img_byte_arr.getvalue()
     return img_byte_arr
 
+
 if __name__ == '__main__':
-    from fight.unit_files import goblin, goblin_bomber
-    from fight import weapons, armors
-    unit = goblin.Goblin()
-    unit1 = goblin_bomber.GoblinBomber()
-    image = create_dungeon_image('D:\YandexDisk\Veganwars\Veganwars\\files\images\\backgrounds\default.jpg', (unit1.get_image(), unit.get_image()))
+    from fight.unit_files.human import Human
+    from fight.unit_files.goblin import Goblin
+    from fight import weapons
+    unit = Human()
+    unit.weapon = weapons.Knife()
+    unit1 = Goblin()
+    print(*unit.get_image())
+    constructor = DuelImage(ImageBackground(Image.open('D:\YandexDisk\Veganwars\Veganwars\\files\images\\backgrounds\Duel.png')),
+                                                   (unit.get_image(), unit1.get_image()))
+    image = constructor.create_image()
+
     image.save('test.png', 'PNG')
