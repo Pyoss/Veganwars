d628fbe9163a72e6159232f48006d9eb289d1158
Pyoss <yisgit@gmail.com>
2019-06-13 17:30:04 +0300

    First dungeon creation.

--- fight/fight_main.py
+++ fight/fight_main.py
@@ -14,8 +14,8 @@ def run_fight(*args, chat_id=None):
     # В качестве аргумента должны быть переданы словари команд в виде
     # [team={chat_id:(name, unit_dict)} or team={ai_class:(ai_class.name, unit_dict)}].
     fight = Fight(chat_id=chat_id)
-    bot_methods.err('test')
     fight.form_teams(args)
+    bot_methods.err('test')
     results = fight.run()
     return results
 
