16db538bfe50cb8c10ce5c74f878ca445749f941
Pyoss <yisgit@gmail.com>
2019-07-18 16:57:44 +0300

    First dungeon creation.

--- bot_utils/bot_methods.py
+++ bot_utils/bot_methods.py
@@ -37,7 +37,7 @@ def delete_message(chat_id=None, message_id=None, call=None):
 
 
 def err(text):
-    admin_bot.send_message(config.admin_id, text, parse_mode=None)
+    admin_bot.send_message(config.admin_id, str(text), parse_mode=None)
 
 
 def get_chat_administrators(chat_id):
