3e80f294f7b6d73a366bcce8b611676e561f6bf9
Pyoss <yisgit@gmail.com>
2019-07-24 12:46:41 +0300

    First dungeon creation.

--- adventures/locations.py
+++ adventures/locations.py
@@ -327,7 +327,7 @@ class RedOak(OpenLocation):
     standard_mobs = False
 
     def create_images(self):
-        self.mob_image = self.image
+        self.mob_image = self.image_file
 
     def get_mobs(self):
         self.mobs = map_engine.MobPack('red_oak', complexity=self.complexity)
