c49ccd4a30c2f390f7c55c390bc9c66b27d6671c
Pyoss <yisgit@gmail.com>
2019-12-04 14:05:49 +0300

    Sending tokens to enviroments

--- bot_utils/bot_methods.py
+++ bot_utils/bot_methods.py
@@ -10,7 +10,7 @@ types = telebot.types
 bot = telebot.TeleBot(config.token)
 admin_bot = telebot.TeleBot(os.environ['ADMIN_TOKEN'])
 admin_mode = False
-images = False
+images = True
 
 
 # Переопределение метода отправки сообщения (защита от ошибок)
