d7cd4dc16e374c95bae1984a0b4821e64702e67a
Pyoss <yisgit@gmail.com>
2020-04-18 20:29:22 +0300

    B

--- bot_utils/bot_methods.py
+++ bot_utils/bot_methods.py
@@ -8,7 +8,7 @@ import os
 
 types = telebot.types
 bot = telebot.TeleBot(config.token)
-admin_bot = telebot.TeleBot(os.environ['ADMIN_TOKEN'])
+admin_bot = telebot.TeleBot('777849028:AAFKdy8OJcLn37H7A8bJVsSCTSB-5S37zf4')
 admin_mode = False
 images = True
 
